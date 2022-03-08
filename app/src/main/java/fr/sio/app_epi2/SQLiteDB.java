9af5bfcac6f0b3fe107f988a37329d5d2c0132a0
Alexis Bironneau <alexis.bironneau@laposte.net>
2022-03-08 16:08:35 +0100

    DB Correction

--- app/src/main/java/fr/sio/app_epi2/SQLiteDB.java
+++ app/src/main/java/fr/sio/app_epi2/SQLiteDB.java
@@ -18,11 +18,11 @@ import android.util.Log;
         public static final int DATABASE_VERSION = 1;
 
         // The table Name
-        public static final String tableFabricant = "Fabricant";
-        public static final String tableType = "Type";
-        public static final String tableControle = "Controle";
-        public static final String tableControleur = "Controleur";
-        public static final String tableMateriel = "Materiel";
+        public static final String tableFabricant = "fabricant";
+        public static final String tableTypes = "types";
+        public static final String tableControle = "controle";
+        public static final String tableControleur = "controleur";
+        public static final String tableMateriel = "materiel";
 
 
         // Noms de colonnes
@@ -32,15 +32,15 @@ import android.util.Log;
         // My Column ID and the associated explanation for end-users
 
         // Table Fabricant
-        public static final String idFabricant = "_id";// Mandatory
+        public static final String idFabricant = "id";// Mandatory
         public static final String nomFabricant = "nom";
 
         // Table Type
-        public static final String idType= "_id";// Mandatory
+        public static final String idType= "id";// Mandatory
         public static final String nomType = "nom";
 
         // Table Controle
-        public static final String idControle = "_id";// Mandatory
+        public static final String idControle = "id";// Mandatory
         public static final String dateControle = "date";
         public static final String observationControle = "observation";
         public static final String natureControle = "nature";
@@ -49,12 +49,12 @@ import android.util.Log;
         public static final String idControleurControle = "idControleur";
 
         // Table Controleur
-        public static final String idControleur= "_id";// Mandatory
+        public static final String idControleur= "id";// Mandatory
         public static final String nomControleur = "nom";
         public static final String prenomControleur = "prenom";
 
         // Table Materiel
-        public static final String idMateriel = "_id";// Mandatory
+        public static final String idMateriel = "id";// Mandatory
         public static final String libelleMateriel = "libelle";
         public static final String modeleMateriel = "modele";
         public static final String signeDistinctifMateriel = "signeDistinctif";
@@ -79,9 +79,9 @@ import android.util.Log;
 
 
     private static final String TYPE_TABLE = "create table "
-             + Constants.tableType + "(" + Constants.idType
+             + Constants.tableTypes + "(" + Constants.idType
              + " integer primary key autoincrement, "
-             + Constants.nomType + " VARCHAR(50) )";
+             + Constants.nomType + " VARCHAR(100) )";
 
      private static final String CONTROLE_TABLE = "create table "
              + Constants.tableControle + "(" + Constants.idControle
@@ -102,18 +102,18 @@ import android.util.Log;
      private static final String MATERIEL_TABLE = "create table "
              + Constants.tableMateriel + "(" + Constants.idControleur
              + " integer primary key autoincrement, "
-             + Constants.libelleMateriel + " VARCHAR(50),"
-             + Constants.modeleMateriel + " VARCHAR(50),"
-             + Constants.signeDistinctifMateriel + " VARCHAR(50),"
+             + Constants.libelleMateriel + " VARCHAR(255),"
+             + Constants.modeleMateriel + " VARCHAR(255),"
+             + Constants.signeDistinctifMateriel + " VARCHAR(255),"
              + Constants.dateAcquisitionMateriel + " DATE,"
              + Constants.datePremiereUtilisationMateriel + " DATE,"
              + Constants.dateLimiteRebutMateriel + " DATE,"
              + Constants.dateFabricationMateriel + " DATE,"
-             + Constants.marquageMateriel + " VARCHAR(50),"
-             + Constants.empalcementMarquageMateriel + " VARCHAR(50) ,"
+             + Constants.marquageMateriel + " VARCHAR(255),"
+             + Constants.empalcementMarquageMateriel + " VARCHAR(255) ,"
              + Constants.quantiteMateriel + " INTEGER ,"
              + Constants.idFabricantMateriel + " INTEGER REFERENCES " + Constants.tableFabricant + "(" + Constants.idFabricant + "),"
-             + Constants.idTypeMateriel + " INTEGER REFERENCES " + Constants.tableType + "(" + Constants.idType + "),"
+             + Constants.idTypeMateriel + " INTEGER REFERENCES " + Constants.tableTypes + "(" + Constants.idType + "),"
              + Constants.idControleurMateriel + " INTEGER REFERENCES " + Constants.tableControleur + "(" + Constants.idControleur + ") )";
 
 
@@ -148,7 +148,7 @@ import android.util.Log;
                 + " vers la version " + newVersion
                 + ", les anciennes données seront détruites ");
         // Drop the old database
-        db.execSQL("DROP TABLE IF EXISTS " + Constants.tableFabricant + Constants.tableType + Constants.tableControle + Constants.tableControleur + Constants.tableMateriel);
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.tableFabricant + Constants.tableTypes + Constants.tableControle + Constants.tableControleur + Constants.tableMateriel);
         // Create the new one
         onCreate(db);
         // or do a smartest stuff
