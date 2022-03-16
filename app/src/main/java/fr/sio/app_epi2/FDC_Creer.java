8b491a2c97cdd82d20947e4791adbcb4d03668b7
Serge Sicot <serge.sicot@gmail.com>
2022-03-16 18:22:09 +0100

    Modif xml activity_fdc_creer.xml
    Creation un nouvel intent dans InfoMateriel et implémentation des boutons fiche de controle ce dernier

--- app/src/main/java/fr/sio/app_epi2/FDC_Creer.java
+++ app/src/main/java/fr/sio/app_epi2/FDC_Creer.java
@@ -3,12 +3,28 @@ package fr.sio.app_epi2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class FDC_Creer extends AppCompatActivity {
+public class FDC_Creer extends AppCompatActivity implements View.OnClickListener {
+
+    private Button annuler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fdc_creer);
+
+        annuler = findViewById(R.id.btnAnnuler);
+
+        annuler.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (annuler.isPressed()){
+            this.finish();
+        }
     }
 }
